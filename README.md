451502c5b0d20717c952c031455d7fe5ccb31483
Zoheb Sumair <syedzoheb1242@gmail.com>
2022-08-24 14:36:41 +0530

    Initial commit for personal site

---
+++ README.md
@@ -0,0 +1,11 @@
+# Personal Site
+
+This is my personal site having blog and my personal information
+
+## Acknowledgment
+
+This theme is based on [Astro Cactus](https://github.com/chrismwilliams/astro-theme-cactus)
+
+## License
+
+Licensed under the MIT License, Copyright © 2022
---
+++ src/site-meta.config.ts
@@ -0,0 +1,11 @@
+const siteMeta = {
+  title: "Syed Zoheb Ali Sumair | Front end developer | ReactJs Developer",
+  description: "ReactJS developer",
+  lang: "en-GB",
+  ogLocale: "locale",
+  githubUrl: "https://github.com/zsumair",
+  themeColorLight: "#fafafa",
+  themeColorDark: "#1d1f21",
+}
+
+export default siteMeta;
